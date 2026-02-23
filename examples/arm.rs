f2e75f1fda926f792a701dde05c76c31f3cea540
Suman <sumansangit.sahu2022@vitstudent.ac.in>
2026-02-23 16:42:27 +0530

    Update arm.rs
    Refactor arm and set_offboard methods to use send function for publishing messages.

--- examples/arm.rs
+++ examples/arm.rs
@@ -1,6 +1,3 @@
-//This thing only arms and runs the drone 1
-
-
 use rclrs::*;
 use px4_msgs::msg::VehicleCommand;
 use std::sync::Arc;
@@ -11,30 +8,34 @@ pub struct ArmDrone {
 
 impl ArmDrone {
     pub fn new(node: &Node) -> Result<Self, RclrsError> {
-        Ok(Self{
+        Ok(Self {
             pub_arm: Arc::new(
                 node.create_publisher("/px4_1/fmu/in/vehicle_command")?
             ),
         })
     }
 
-    pub fn arm(&self) {
-        let mut msg = VehicleCommand::default();
-        msg.command = VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM as u32;   //u16 -> u32
-        msg.param1 = 1.0;
-        msg.target_system = 2;   //only drone1
+    fn send(&self, mut msg: VehicleCommand) {
+        msg.target_system = 2;
+        msg.target_component = 1;
+        msg.source_system = 1;
+        msg.source_component = 1;
         msg.from_external = true;
         self.pub_arm.publish(&msg).ok();
     }
 
+    pub fn arm(&self) {
+        let mut msg = VehicleCommand::default();
+        msg.command = VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM as u32;
+        msg.param1 = 1.0;
+        self.send(msg);
+    }
+
     pub fn set_offboard(&self) {
         let mut msg = VehicleCommand::default();
-        msg.command = VehicleCommand::VEHICLE_CMD_DO_SET_MODE as u32;   //u16 -> u32
+        msg.command = VehicleCommand::VEHICLE_CMD_DO_SET_MODE as u32;
         msg.param1 = 1.0;
         msg.param2 = 6.0;
-        msg.target_system = 2;   //only drone1
-        msg.from_external = true;
-        self.pub_arm.publish(&msg).ok();
-
+        self.send(msg);
     }
 }
