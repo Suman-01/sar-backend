623a0fc726aa43d3fe75efcda61b238c4c5212ce
Suman <sumansangit.sahu2022@vitstudent.ac.in>
2026-02-25 15:00:57 +0530

    1st working draft

--- examples/arm.rs
+++ examples/arm.rs
@@ -1,22 +1,28 @@
+// arm.rs
 use rclrs::*;
 use px4_msgs::msg::VehicleCommand;
 use std::sync::Arc;
 
+#[derive(Clone)]
 pub struct ArmDrone {
     pub_arm: Arc<Publisher<VehicleCommand>>,
+    system_id: u8,
 }
 
 impl ArmDrone {
-    pub fn new(node: &Node) -> Result<Self, RclrsError> {
+    /// new(node, namespace, system_id)
+    /// namespace like "px4_1" or "px4_2"
+    pub fn new(node: &Node, ns: &str, system_id: u8) -> Result<Self, RclrsError> {
+        let topic = format!("/{}/fmu/in/vehicle_command", ns);
         Ok(Self {
-            pub_arm: Arc::new(
-                node.create_publisher("/px4_1/fmu/in/vehicle_command")?
-            ),
+            pub_arm: Arc::new(node.create_publisher(topic.as_str())?),
+            system_id,
         })
     }
 
     fn send(&self, mut msg: VehicleCommand) {
-        msg.target_system = 2;
+        // set correct target system from constructor
+        msg.target_system = self.system_id as u8;
         msg.target_component = 1;
         msg.source_system = 1;
         msg.source_component = 1;
@@ -31,11 +37,24 @@ impl ArmDrone {
         self.send(msg);
     }
 
+    pub fn disarm(&self) {
+        let mut msg = VehicleCommand::default();
+        msg.command = VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM as u32;
+        msg.param1 = 0.0;
+        self.send(msg);
+    }
+
     pub fn set_offboard(&self) {
         let mut msg = VehicleCommand::default();
         msg.command = VehicleCommand::VEHICLE_CMD_DO_SET_MODE as u32;
         msg.param1 = 1.0;
         msg.param2 = 6.0;
+        self.send(msg);
+    }
+
+    pub fn land(&self) {
+        let mut msg = VehicleCommand::default();
+        msg.command = VehicleCommand::VEHICLE_CMD_NAV_LAND as u32;
         self.send(msg);
     }
 }
