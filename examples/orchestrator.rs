f60c7eea1eef2e2c1c1bfccd58c0b8126beb9e20
Suman <sumansangit.sahu2022@vitstudent.ac.in>
2026-02-25 15:04:49 +0530

    1st working draft

--- examples/orchestrator.rs
+++ examples/orchestrator.rs
@@ -1,3 +1,5 @@
+// orchestrator.rs
+
 mod offboard;
 mod arm;
 mod take_off;
@@ -9,48 +11,52 @@ use std::thread;
 
 use offboard::OffboardController;
 use arm::ArmDrone;
-use take_off::TakeOff;
 use mission::run_mission;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    // =====================
-    // ROS SETUP
-    // =====================
     let context = Context::default_from_env()?;
     let mut executor = context.create_basic_executor();
 
-    let controller = Arc::new(OffboardController::new(&executor)?);
+    // create offboard controllers for both namespaces
+    let controller1 = Arc::new(OffboardController::new(&executor, "px4_1")?);
+    let controller2 = Arc::new(OffboardController::new(&executor, "px4_2")?);
+
+    // single node for mission/arming topics
     let node = executor.create_node("mission_controller")?;
 
-    let arm = ArmDrone::new(&node)?;
+    let arm1 = Arc::new(ArmDrone::new(&node, "px4_1", 2)?);
+    let arm2 = Arc::new(ArmDrone::new(&node, "px4_2", 3)?);
 
-    // =====================
-    // OFFBOARD THREAD
-    // =====================
-    let ctrl_clone = controller.clone();
-    thread::spawn(move || {
-        ctrl_clone.spin_loop();
-    });
 
-    // =====================
-    // MISSION THREAD
-    // =====================
-    let ctrl_m = controller.clone();
+    {
+        let c = controller1.clone();
+        thread::spawn(move || {
+            c.spin_loop();
+        });
+    }
+    {
+        let c = controller2.clone();
+        thread::spawn(move || {
+            c.spin_loop();
+        });
+    }
+
+
+    let ctrl1 = controller1.clone();
+    let ctrl2 = controller2.clone();
     let node_m = node.clone();
+    let arm1_m = arm1.clone();
+    let arm2_m = arm2.clone();
 
     thread::spawn(move || {
-
+        // small delay so everything is up
         thread::sleep(std::time::Duration::from_secs(1));
 
-        arm.set_offboard();
-        arm.arm();
-
-        // TAKEOFF
-        TakeOff::takeoff(&ctrl_m);
-
-        // MISSION
-        run_mission(&node_m, &ctrl_m).ok();
+        // now run the coordinated mission in mission.rs
+        if let Err(e) = run_mission(&node_m, ctrl1, ctrl2, arm1_m, arm2_m) {
+            eprintln!("Mission error: {:?}", e);
+        }
     });
 
     // =====================
